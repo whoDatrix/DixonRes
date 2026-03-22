7d15d9cef5fa4fb49487e29f8bf1d50588ee120b
whoDatrix <suohaohai@gmail.com>
2026-03-22 15:08:10 +0800

    Update fq_unified_interface.h

--- include/fq_unified_interface.h
+++ include/fq_unified_interface.h
@@ -17,10 +17,7 @@ extern "C" {
 #endif
 
 #if __FLINT_VERSION < 3 || (__FLINT_VERSION == 3 && __FLINT_VERSION_MINOR < 1)
-  static inline ulong fq_nmod_ctx_prime_ui(const fq_nmod_ctx_t ctx) {
-      return fmpz_get_ui(fq_nmod_ctx_prime(ctx));
-  }
-  #define fq_nmod_ctx_prime fq_nmod_ctx_prime_ui
+  #define fq_nmod_ctx_prime(ctx) fmpz_get_ui(fq_nmod_ctx_prime(ctx))
 #endif
 
 #if __FLINT_VERSION < 3 || (__FLINT_VERSION == 3 && __FLINT_VERSION_MINOR < 2)
